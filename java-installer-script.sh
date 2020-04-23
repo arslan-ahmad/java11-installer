142094aaf998e5aa7030b8f145edc92969f8c677
Arslan Ahmad <arslan.ahmad.sh@gmail.com>
2020-04-23 16:02:12 +0200

    Update java-installer-script.sh

--- java-installer-script.sh
+++ java-installer-script.sh
@@ -10,5 +10,5 @@ sudo cp jdk-11.0.6_linux-x64_bin.tar.gz /var/cache/oracle-jdk11-installer-local/
 #update 
 sudo apt-get update
 sudo apt-get install oracle-java11-installer-local -y
-sudo apt-get install oracle-java11-set-default-local
+sudo apt-get install oracle-java11-set-default-local -y
 

b00d444c0822950be3f69b615ef6ae6e5757b159
Arslan Ahmad <arslan.ahmad.sh@gmail.com>
2020-04-23 13:48:40 +0200

    Update and rename script.sh to java-installer-script.sh

---
+++ java-installer-script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#adding repository in ppa list
+sudo add-apt-repository ppa:linuxuprising/java
+#downloading jdk package for the installation
+wget https://files01.tchspt.com/temp/jdk-11.0.6_linux-x64_bin.tar.gz
+#copying package for the installation of the JDK locally 
+sudo mkdir -p /var/cache/oracle-jdk11-installer-local
+sudo cp jdk-11.0.6_linux-x64_bin.tar.gz /var/cache/oracle-jdk11-installer-local/
+#update 
+sudo apt-get update
+sudo apt-get install oracle-java11-installer-local -y
+
--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo add-apt-repository ppa:linuxuprising/java
-wget 
-sudo apt-get update
